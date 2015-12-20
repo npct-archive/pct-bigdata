ee6a5eae1132ad25e0421a4dd06524deaa64dd92
robinlovelace <rob00x@gmail.com>
2015-12-20 18:34:23 +0000

    Simplify - mv national/* .

--- coventry/server.R
+++
@@ -1,3 +0,0 @@
-data_dir <- "../pct-data-master/coventry/"
-source("../master/master.R", local = T, chdir = T)
-
--- norwich/server.R
+++
@@ -1,3 +0,0 @@
-data_dir <- "../pct-data-master/norwich/"
-source("../master/master.R", local = T, chdir = T)
-
